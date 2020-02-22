2cdbf8db42e644d72f154a20c614d93ddc913c5a
Leah Weston <westonl91@yahoo.com>
2020-02-22 15:25:55 -0800

    halfway through tutorial part 3

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,35 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
 
 from django.http import HttpResponse
+#from django.template import loader
+from .models import Question
 
 
 def index(request):
-        return HttpResponse("Hello, everyone. I'm Leah. Can I put <h1>HTML</h1> in here?")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    context = {
+            'latest_question_list': latest_question_list
+    }
+   # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist, yet...")
+    return render(request, 'polls/detail.html', {'question': question})
+    #return HttpResponse("This is question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
